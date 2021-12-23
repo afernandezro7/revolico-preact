fd15cd0314ab2ed0e894f87a0f32d32b36ecf4c3
Alberto Fernandez <a.fernandez.ro7@gmail.com>
2021-12-23 09:10:15 +0100

    adding cart animations

--- src/components/header/CarItems.js
+++ src/components/header/CarItems.js
@@ -1,16 +1,29 @@
 import { h } from 'preact';
-import { useContext } from 'preact/hooks';
+import { useContext, useEffect, useLayoutEffect, useRef } from 'preact/hooks';
 import { BsCart3 } from "react-icons/bs";
 import { CarContext } from '../../hooks/carContext';
 import style from './style.css';
+import { gsap } from 'gsap'
 
 export const CarItems = () => {
 
-    const { state } = useContext(CarContext)
+    const { state:{count} } = useContext(CarContext)
+    const countRef = useRef(null)
+    const tl = useRef(gsap.timeline())
+
+    useLayoutEffect(() => {
+        tl.current.to(countRef.current, {y: -10 , duration: 0.2 , ease: 'ease.out'})
+                  .to(countRef.current, {y: 0 , duration: 1 , ease: 'bounce.out'})
+                  .pause();
+    }, [])
+
+    useEffect(() => {
+       tl.current.play(0)
+    }, [count])
 
     return (
         <button type="button" class={style.btn}>
-            <BsCart3 /><span class={style.badge}>{state.count}</span>
+            <BsCart3 /><span ref={countRef} class={style.badge}>{count}</span>
         </button>
     );
 }
